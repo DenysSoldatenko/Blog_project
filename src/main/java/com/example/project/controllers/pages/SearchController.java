7a685c4abb30ecbcbf0d0367d0fef5da884a0e47
DenysSoldatenko <soldatenko911@gmail.com>
2023-08-10 22:18:49 +0300

    Added search functionality

--- src/main/java/com/example/project/controllers/pages/SearchController.java
+++ src/main/java/com/example/project/controllers/pages/SearchController.java
@@ -1,11 +1,15 @@
 package com.example.project.controllers.pages;
 
 import com.example.project.controllers.AbstractController;
+import com.example.project.entities.Article;
+import com.example.project.models.Items;
+import com.example.project.utils.Constant;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * Controller for handling requests related to the "Search" page.
@@ -15,6 +19,16 @@ public class SearchController extends AbstractController {
   @Override
   protected void doGet(HttpServletRequest req,
                        HttpServletResponse resp) throws ServletException, IOException {
-    forwardToPage("search.jsp", req, resp);
+    String query = req.getParameter("query");
+    if (StringUtils.isNotBlank(query)) {
+      Items<Article> items = getBusinessService().listArticlesBySearchQuery(query, 0,
+          Constant.LIMIT_ARTICLES_PER_PAGE);
+      req.setAttribute("list", items.getItems());
+      req.setAttribute("count", items.getCount());
+      req.setAttribute("searchQuery", query);
+      forwardToPage("search.jsp", req, resp);
+    } else {
+      resp.sendRedirect("/news");
+    }
   }
 }
